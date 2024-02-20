61f4ee6158c8960a9d2751376498d6d771e550ed
wuliao97 <117802193+wuliao97@users.noreply.github.com>
2024-02-20 14:24:46 +0000

    first commit

---
+++ display/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn dbg(val: usize) {
+    dbg!(val);
+}
+
+pub fn calc_and_dbg(left: usize, right: usize) {
+    dbg!(left + right);
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// use calc::
+use display::calc_and_dbg;
+
+fn main() {
+
+    calc_and_dbg(12, 38);
+}
